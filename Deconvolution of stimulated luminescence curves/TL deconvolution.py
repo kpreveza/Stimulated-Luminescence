15f0181fc53c86f2d8044acac9b8420eb86914df
Konstantina Prevezanou <82606741+kpreveza@users.noreply.github.com>
2021-12-01 14:45:34 +0200

    Add files via upload

---
+++ Deconvolution of stimulated luminescence curves/TL deconvolution.py
@@ -0,0 +1,365 @@
+		#########################################
+		#                                       #
+		#   Deconvolution of TL curves          #
+		#                                       #
+		#   Author: Prevezanou Konstantina      #
+		#                                       #
+		#   E-mail: kpreveza@physics.auth.gr    #
+		#                                       #
+		#########################################
+
+#LIBRARIES
+
+'''We import the libraries that we are going to use'''
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import scipy.constants
+from scipy.special import lambertw, exp1
+from lmfit import Parameters, report_fit, Parameter, Model
+import pybroom as br
+import pandas as pd
+from easygui import *
+from tkinter import *
+from tkinter import filedialog,ttk
+import os
+from os.path import exists
+
+
+#FUNCTIONS
+
+k=scipy.constants.physical_constants['Boltzmann constant in eV/K']
+K=k[0]
+
+def tl(x,Im,Tm,E,R):
+	'''Theoretical function for the TL phenomenon'''
+	c=(1-R)/R
+	F=x*np.exp(-E/(K*x))+(E/K)*(- np.real(exp1(E/(K*x))))
+	Z=1/c-np.log(c)+(E*np.exp(E/(K*Tm))*F)/(K*(Tm**2)*(1-1.05*(R**1.26)))
+	#W=np.real(lambertw(np.exp(Z)))
+	Fm=Tm*np.exp(-E/(K*Tm))+(E/K)*(- np.real(exp1(E/(K*Tm))))
+	Zm=1/c-np.log(c)+(E*np.exp(E/(K*Tm))*Fm)/(K*(Tm**2)*(1-1.05*(R**1.26)))
+	Wm=np.real(lambertw(np.exp(Zm)))
+
+	for i in range(len(Z)):
+		if Z[i]<710:
+			W=np.real(lambertw(np.exp(Z)))
+		else:
+			W=Z-np.log(Z)
+		return Im*((Wm+Wm**2)/(W+W**2))*np.exp((E*(x-Tm))/(K*x*Tm))
+
+
+def freq_f(Tm,E,R):
+	'''Function for the computation of the frequency factor'''
+	c=(1-R)/R
+	F_m=((K*(Tm**2))/E)*np.exp(-E/(K*Tm))*(1-((2*K*Tm)/E))
+	z_m=1/c-np.log(c)+(E*np.exp(E/(K*Tm))*F_m)/(K*(Tm**2)*(1-1.05*(R**1.26)))
+	w=np.real(lambertw(np.exp(z_m)))
+	return ((1-R)*(1+w)**2*heat_rate*E)/((1+2*w)*(K*Tm**2*np.exp(-E/(K*Tm))))
+
+
+def make_model(num):
+	'''We define the model using the theoretical function and define the parameters'''
+	pref = "tl{}_".format(num)
+	peak = list_peaks[num]
+	model = Model(tl, prefix = pref)
+	model.set_param_hint(pref+'Im', value = a[0][num], min = a[1][num], max = a[2][num])
+	model.set_param_hint(pref+'Tm', value = b[0][num], min = b[1][num], max = b[2][num])
+	model.set_param_hint(pref+'E', value = c[0][num], min = c[1][num], max = c[2][num])
+	model.set_param_hint(pref+'R', value = d[0][num], min = d[1][num], max = d[2][num])
+	return model
+
+
+def loop_init(name1,name2,name3,num):
+	'''Function to make the list for each parameter including the initial parameters'''
+	name1=[]
+	name2=[]
+	name3=[]
+	for i in range(num,len(datap),5):
+		name1.append(datap[i][0])
+		name2.append(datap[i][1])
+		name3.append(datap[i][2])
+	name1=np.array(name1,dtype=float)
+	name2=np.array(name2,dtype=float)
+	name3=np.array(name3,dtype=float)
+	return name1,name2,name3
+
+
+def file_exist(f):
+	'''Function to check if a file exists and if it does it adds a number at the end of the name'''
+	fnew = f
+	root, ext = os.path.splitext(f)
+	i = 0
+	while os.path.exists(fnew):
+		i += 1
+		fnew = '%s_%i%s' % (root, i, ext)
+	return fnew
+
+
+#INPUT DATA
+
+'''Info and input from user about experimental data, we use the tkinter method to make a pop-up box'''
+root=Tk()
+root.title('Data for the deconvolution')
+root.geometry('700x370')
+
+global b_sequence, files, fileswithoutpath
+b_sequence=[]
+files=[]
+fileswithoutpath=[]
+
+
+def addfile():
+	'''Function to go through computer files'''
+	filename=filedialog.askopenfilename(initialdir=os.getcwd(),title="Select File",filetypes=(('TXT files','.txt'),('All files','.')))
+	files.append(filename)
+	filenamewithout=os.path.split(filename)[1]
+	fileswithoutpath.append(filenamewithout)
+
+def which_button(t):
+	'''Function to save the order of button push'''
+	b_sequence.append(t)
+
+def clickonce(k):
+	'''Function to allow the user to press the button only once'''
+	k.configure(state=DISABLED)
+
+notice=Label(root,text="What you should know to run the script properly:",font=('Arial',10, 'underline'))
+notice.pack()
+
+info=Label(root,text="\n1. You need one file with experimental data (first column index in ascending order, second column x-axis data,\nthird column y-axis data) in ‘txt (tab delimited)’ formating.\n\n2. You need a second file with the initial guess of the parameters in ‘txt (tab delimited)’ formating.\nThe format should be: three columns - first contains the values, second the minimum and third the maximum for each value.\nAlso, for each peak the parameters (Im, Tm, E, R) will be given and between the different peaks should be a blank line.\n\n3. Read carefully the instructions of each pop-up box to correctly complete the gaps.\n\n4. At the end, fitted parameters will be stored in a new directory, ‘Fitted parameters’,\nand theoretical data in new directory, ‘Theoretical data’.\n\n5. To save the graph just click on ‘Save’ (the button is on the down left corner of the graph).\n\n")
+info.pack()
+
+out=Button(root,text='Continue',padx=5,pady=4,fg='white',bg='black',command=root.destroy ,font=('Arial', 11))
+out.pack(side=BOTTOM)
+
+frame1=Frame(root)
+frame1.place(relwidth=0.41,relheight=0.1,relx=0.3,rely=0.73)
+
+expdata=Button(frame1,text='Experimental data',padx=6,pady=5,fg='white',bg='black',command=lambda:[addfile(),which_button('exp'),clickonce(expdata)],font=('Arial', 11))
+expdata.pack(side=LEFT)
+
+expparams=Button(frame1,text='Initial parameters',padx=6,pady=5,fg='white',bg='black',command=lambda:[addfile(),which_button('para'),clickonce(expparams)],font=('Arial', 11))
+expparams.pack(side=RIGHT)
+
+root.mainloop()
+
+'''We import the experimental data'''
+if b_sequence[0]=='exp':
+	#Import experimental data
+	with open (files[0], 'r') as file:
+		data=list(csv.reader(file,delimiter='\t'))
+
+	usedata=np.array(data[1:], dtype=float)
+	x=usedata[:,1]
+	y=usedata[:,2]
+
+
+	#Import parameters
+	with open (files[1], 'r') as filep:
+		datap=list(csv.reader(filep,delimiter='\t'))
+		
+	a = loop_init('Iinit','minIm','maxIm',1)
+	b = loop_init('Tinit','minTm','maxTm',2)
+	c = loop_init('Einit','minE','maxE',3)
+	d = loop_init('Rinit','minR','maxR',4)
+
+	'''We create the names of the output files'''
+	file1=fileswithoutpath[0].replace('.txt','')
+
+	newfile1=file1+'_out.txt'
+	newfile2=file1+'_param.txt'
+else:
+	#Import experimental data
+	with open (files[1], 'r') as file:
+		data=list(csv.reader(file,delimiter='\t'))
+
+	usedata=np.array(data[1:], dtype=float)
+	x=usedata[:,1]
+	y=usedata[:,2]
+
+
+	#Import parameters
+	with open (files[0], 'r') as filep:
+		datap=list(csv.reader(filep,delimiter='\t'))
+	
+	a = loop_init('Iinit','minIm','maxIm',1)
+	b = loop_init('Tinit','minTm','maxTm',2)
+	c = loop_init('Einit','minE','maxE',3)
+	d = loop_init('Rinit','minR','maxR',4)
+
+	'''We create the names of the output files'''
+	file1=fileswithoutpath[1].replace('.txt','')
+
+	newfile1=file1+'_out.txt'
+	newfile2=file1+'_param.txt'
+
+
+'''User tells the number of peaks, the start+end of the deconvolution and the heating rate for the calculation of the frequency factor'''
+msg = "Write down how many peaks you want to use, the start and end number.\nProvide also the heating rate."
+title = "Data for deconvolution"
+fieldNames = ['Number of peaks',"Start number",'End number','Heating rate']
+fieldValues = []
+fieldValues = multenterbox(msg,title,fieldNames)
+
+peaks=int(fieldValues[0])
+list_peaks=range(peaks)
+startfit = int(fieldValues[1])
+endfit = int(fieldValues[2])
+heat_rate=float(fieldValues[3])
+
+'''We define the start and end of the experimental data that we are going to use'''
+x=np.array(x[startfit:endfit])
+y=np.array(y[startfit:endfit])
+
+
+#DECONVOLUTION
+
+'''Run the model to make the total peak'''
+mod = None
+for i in range(len(list_peaks)):
+	this_mod = make_model(i)
+	if mod is None:
+		mod = this_mod
+	else:
+		mod = mod + this_mod
+
+
+'''Choose method for deconvolution'''
+msg = 'Choose one method to perform the deconvolution.\n\n1. Levenberg Marquardt - leastsq\n2. Nelder Mead - nelder\n3. Powell - powell'
+title = "Deconvolution method"
+choices = ['leastsq','nelder','powell']
+choice = choicebox(msg, title, choices)
+
+howtofit=choice
+
+'''Deconvolution'''
+out=mod.fit(y, x=x, method=howtofit)#,nan_policy='omit')
+
+
+#NEW DIRECTORIES
+
+'''Creating new directories to save seperately the output files'''
+if not os.path.exists('Fitted parameters'):
+	os.mkdir('Fitted parameters') 
+else:    
+	pass
+
+if not os.path.exists('Theoretical data'):
+	os.mkdir('Theoretical data')
+else:    
+	pass
+
+
+#FILES
+
+'''File with theoretical data for each and total peak'''
+da = br.augment(out)
+da=da.round(3)
+da.to_csv(file_exist("Theoretical data/{}".format(newfile1)),sep='\t')
+
+
+'''Calculation of total FOM'''
+residual = da['residual'].to_numpy()
+yfitdata= da['best_fit'].to_numpy()
+fom=round(100*(np.sum(abs(residual))/(np.sum(yfitdata))),3)
+Fom=['' for i in range(peaks)]
+Fom[0]=fom
+
+
+'''Parameters file creation'''
+dt=br.tidy(out)
+values=dt['value'].to_numpy()
+
+If=[]
+Tf=[]
+Ef=[]
+Rf=[]
+name_peaks=[]
+
+for i in range(1,peaks+1):
+	name_peaks.append('Peak_{}'.format(i))
+for i in range(0,len(dt),4):
+	Ef.append(values[i])
+for i in range(1,len(dt),4):
+	If.append(values[i])
+for i in range(2,len(dt),4):
+	Rf.append(values[i])
+for i in range(3,len(dt),4):
+	Tf.append(values[i])
+
+'''Frequency factor calculation'''
+frequency_factor=[]
+for i in range(peaks):
+	s=freq_f(Tf[i],Ef[i],Rf[i])
+	frequency_factor.append(s)
+
+
+df = pd.DataFrame(list(zip(name_peaks,If,Tf,Ef,Rf,frequency_factor,Fom)),columns =['Peaks', 'Imax','Tmax','E','R','s','Total Fom'])
+#df=df.round(3)
+df.to_csv(file_exist("Fitted parameters/{}".format(newfile2)),sep='\t')
+
+'''New dataframe with fitted parameters thw will be used for the seperate peak files'''
+dk=pd.DataFrame(list(zip(name_peaks,If,Tf,Ef,Rf,frequency_factor)),columns =['Peaks', 'Imax','Tmax','E','R','s'])
+dk=dk.round(3)
+
+
+'''Peaks to seperate files'''
+msg = 'Do you want to save each peak in a seperate file?'
+title = 'Seperate files'
+choices = ['Yes','No']
+choice = choicebox(msg, title, choices)
+
+seper_file=choice
+
+if seper_file=='No':
+	pass
+else:
+	'''Extra condition to import'''
+	msg = "What is the experimental condition (ex. Dose, Temperature) ?"
+	title = "Condition"
+	fieldNames = ['Value']
+	fieldValues = []
+	fieldValues = multenterbox(msg,title,fieldNames)
+
+	column_vl=fieldValues[0]
+
+	dk['Condition'] = column_vl
+
+	'''Seperate files creation'''
+	for i in range(peaks):
+		if exists('Fitted parameters/Peak_{}_{}.txt'.format(i+1,file1))==True:
+			with open ('Fitted parameters/Peak_{}_{}.txt'.format(i+1,file1), 'a') as filep:
+				filep.write('\n')
+				filep.write('{}'.format(i)+'  '+str(dk.loc[i,'Peaks'])+'  '+str(dk.loc[i,'Imax'])+'  '+str(dk.loc[i,'Tmax'])+'  '+str(dk.loc[i,'E'])+'  '+str(dk.loc[i,'R'])+'  '+str(dk.loc[i,'s'])+'  '+str(dk.loc[i,'Condition']))
+		else:
+			with open ('Fitted parameters/Peak_{}_{}.txt'.format(i+1,file1), 'w') as filep:
+				filep.write(str(dk.loc[[i]]))
+
+
+#OUTPUT
+
+'''Print fitted parameters and FOM'''
+print('\nTotal FOM is: ', fom, ' %.\n')
+out.params.pretty_print(columns=['value', 'min', 'max','init_value'])
+
+
+'''Plot each peak'''
+i=0
+for i in range(peaks):
+	ytl=da["Model(tl, prefix='tl{}_')".format(i)].to_numpy()
+	plt.plot(x,ytl,label='Peak {}'.format(i+1))
+	
+
+'''Plot theoretical and experimental total peak'''
+plt.plot(x, out.best_fit,label='Theoretical Data, \nFOM = {}'.format(fom),color='k')
+plt.scatter(x,y,label='Experimental Data',marker=".",color='r')
+plt.title('TL Data')
+plt.xlabel(data[0][1])
+plt.ylabel(data[0][2])
+plt.grid(True)
+plt.tight_layout()
+#plt.legend()
+plt.show()
+
+#plt.savefig('G:/My Drive/Paper LED 2021/TL/tldata.eps', format='eps')
